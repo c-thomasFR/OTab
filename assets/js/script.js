a661f09ac2fafdf18522238cb0731629a62f38c7
c-thomasFR <contact@c-thomas.fr>
2019-08-23 19:40:15 +0200

    Add color picker to change the color of the header

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ function compare(a,b) {
         return 1;
     return 0;
 }
-
 function detectExtension(extensionId, file, callback) 
 { 
     var img; 
@@ -18,7 +17,7 @@ function detectExtension(extensionId, file, callback)
       callback(false); 
     };
 }
-
+// Function permettant de lancer un extension
 function launchExtension(extensionId) 
 { 
     chrome.tabs.query({currentWindow: true, active: true}, function (tab) 
@@ -34,7 +33,8 @@ function launchExtension(extensionId)
     });
 
 }
-
+// Function de validation du form dans la modal modalSetting de l'extension
+// Enregistre dans le localStorage le nom du fichier pour lancer l'extension
 (function() {
     'use strict';
     window.addEventListener('load', function() 
@@ -63,6 +63,8 @@ function launchExtension(extensionId)
       });
     }, false);
   })();
+
+// Toggle of the dark and du light mode
 function toggleDarkLight() 
 {
     //$('./assets/css/bootstrap-dark.min.css"]').prop('disabled', false);
@@ -82,6 +84,7 @@ function toggleDarkLight()
     checkCssLoadedDark();
 }
 
+// Check if DarkMode or LightMode is loaded to show the right button
 function checkCssLoadedDark()
 {
     if ($('link[href="./assets/css/bootstrap-dark.min.css"]').prop( "disabled" ))
@@ -99,6 +102,130 @@ function checkCssLoadedDark()
 
 document.addEventListener('DOMContentLoaded', function() 
 {
+    const pickr = new Pickr({
+
+        // Selector or element which will be replaced with the actual color-picker.
+        // Can be a HTMLElement.
+        el: '#btn-color-picker',
+    
+        // Where the pickr-app should be added as child.
+        container: 'body',
+    
+        // Which theme you want to use. Can be 'classic', 'monolith' or 'nano'
+        theme: 'nano',
+    
+        // Nested scrolling is currently not supported and as this would be really sophisticated to add this
+        // it's easier to set this to true which will hide pickr if the user scrolls the area behind it.
+        closeOnScroll: false,
+    
+        // Custom class which gets added to the pcr-app. Can be used to apply custom styles.
+        appClass: 'custom-class',
+    
+        // Don't replace 'el' Element with the pickr-button, instead use 'el' as a button.
+        // If true, appendToBody will also be automatically true.
+        useAsButton: false,
+    
+        // If true pickr won't be floating, and instead will append after the in el resolved element.
+        // Setting this to true will also set showAlways to true. It's possible to hide it via .hide() anyway.
+        inline: false,
+    
+        // If true, pickr will be repositioned automatically on page scroll or window resize.
+        // Can be set to false to make custom positioning easier.
+        autoReposition: true,
+    
+        // Defines the direction in which the knobs of hue and opacity can be moved.
+        // 'v' => opacity- and hue-slider can both only moved vertically.
+        // 'hv' => opacity-slider can be moved horizontally and hue-slider vertically.
+        // Can be used to apply custom layouts
+        sliders: 'h',
+    
+        // Start state. If true 'disabled' will be added to the button's classlist.
+        disabled: false,
+    
+        // If true, the user won't be able to adjust any opacity.
+        // Opacity will be locked at 1 and the opacity slider will be removed.
+        // The HSVaColor object also doesn't contain an alpha, so the toString() methods just
+        // print HSV, HSL, RGB, HEX, etc.
+        lockOpacity: true,
+    
+        // Precision of output string (only effective if components.interaction.input is true)
+        outputPrecision: 0,
+    
+        // If set to false it would directly apply the selected color on the button and preview.
+        comparison: true,
+        // Default color
+        default: '#000000',
+        swatches: null,
+    
+        // Default color representation of the input/output textbox.
+        // Valid options are `HEX`, `RGBA`, `HSVA`, `HSLA` and `CMYK`.
+        defaultRepresentation: 'HEX',
+    
+        // Option to keep the color picker always visible.
+        // You can still hide / show it via 'pickr.hide()' and 'pickr.show()'.
+        // The save button keeps its functionality, so still fires the onSave event when clicked.
+        showAlways: false,
+    
+        // Defines the position of the color-picker.
+        // Any combinations of top, left, bottom or right with one of these optional modifiers: start, middle, end
+        // Examples: top-start / right-end
+        // If clipping occurs, the color picker will automatically choose its position.
+        position: 'top-middle',
+    
+        // Enables the ability to change numbers in an input field with the scroll-wheel.
+        // To use it set the cursor on a position where a number is and scroll, use ctrl to make steps of five
+        adjustableNumbers: true,
+    
+        // Show or hide specific components.
+        // By default only the palette (and the save button) is visible.
+        components: {
+    
+            // Defines if the palette itself should be visible.
+            // Will be overwritten with true if preview, opacity or hue are true
+            palette: true,
+    
+            preview: true, // Display comparison between previous state and new color
+            opacity: false, // Display opacity slider
+            hue: true,     // Display hue slider
+    
+            // show or hide components on the bottom interaction bar.
+            interaction: {
+                hex: false,  // Display 'input/output format as hex' button  (hexadecimal representation of the rgba value)
+                rgba: false, // Display 'input/output format as rgba' button (red green blue and alpha)
+                hsla: false, // Display 'input/output format as hsla' button (hue saturation lightness and alpha)
+                hsva: false, // Display 'input/output format as hsva' button (hue saturation value and alpha)
+                cmyk: false, // Display 'input/output format as cmyk' button (cyan mangenta yellow key )
+    
+                input: false, // Display input/output textbox which shows the selected color value.
+                             // the format of the input is determined by defaultRepresentation,
+                             // and can be changed by the user with the buttons set by hex, rgba, hsla, etc (above).
+                cancel: true, // Display Cancel Button, resets the color to the previous state
+                clear: false, // Display Clear Button; same as cancel, but keeps the window open
+                save: true,  // Display Save Button,
+            },
+        },
+    
+        // Button strings, brings the possibility to use a language other than English.
+        strings: {
+           save: 'Save',  // Default for save button
+           clear: 'Clear', // Default for clear button
+           cancel: 'Cancel' // Default for cancel button
+        }
+    });
+
+    pickr.on('save', (color, instance) => 
+    {
+        //console.log('save', color, instance);
+        //console.log(color.toHEXA().toString());
+        document.querySelector('#header-nav').setAttribute('style', 
+        'background-color:'+color.toHEXA().toString()+' !important');
+        localStorage['header-color'] = color.toHEXA().toString();
+    });
+    if (localStorage.getItem('header-color') !== null) 
+    {
+        document.querySelector('#header-nav').setAttribute('style', 
+        'background-color:'+localStorage['header-color']+' !important');
+    }
     var buttonsToggleDark = document.getElementsByName('toggleDark');
     if(localStorage["darkMode"] === undefined)
     {
@@ -118,7 +245,7 @@ document.addEventListener('DOMContentLoaded', function()
             $('link[href="./assets/css/bootstrap-dark.min.css"]').prop('disabled', true);
         }
     }
-    /*checkCssLoadedDark();
+    checkCssLoadedDark();
     buttonsToggleDark[0].onclick = function()
     {
         toggleDarkLight();
@@ -126,7 +253,7 @@ document.addEventListener('DOMContentLoaded', function()
     buttonsToggleDark[1].onclick = function()
     {
         toggleDarkLight();
-    };*/
+    };
     var index = 0;
     var array=[];
     var options = document.getElementById('list_extensions');
@@ -206,6 +333,8 @@ document.addEventListener('DOMContentLoaded', function()
                 });
             }
         });
+
+        // Function pour afficher la modal et remplir le contenu
         $('#modalSetting').on('show.bs.modal', function (event) 
         {
             var button = $(event.relatedTarget); // Button that triggered the modal
@@ -233,6 +362,10 @@ document.addEventListener('DOMContentLoaded', function()
                 e.preventDefault();
                 chrome.tabs.create({url:  $(this).attr('href')});
             });
+        });
+        $('#modalSetting').on('show.bs.modal', function (event) 
+        {
+            
         })
     });
 });
